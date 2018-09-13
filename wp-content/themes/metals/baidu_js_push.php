082f214b2022d792c61a97bfff91b3b82ee80fdd
chaxiaomao <1145470390@qq.com>
2018-09-13 21:59:47 +0800

    0

---
+++ wp-content/themes/metals/baidu_js_push.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jemi
+ * Date: 2018/9/13
+ * Time: 21:58
+ */
+
+?>
+<script>
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https'){
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else{
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+</script>
+
+
